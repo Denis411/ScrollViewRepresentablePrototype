964044e0ae4115771ecd5b4656bc7b2efcda3ab2
Denis Ulianov <dulianov@tradingview.com>
2023-11-30 18:26:01 +0400

    Initial Commit

---
+++ ScrollViweUIViewRepresentable/ScrollViweUIViewRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViweUIViewRepresentableApp.swift
+//  ScrollViweUIViewRepresentable
+//
+//  Created by dulianov on 30.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViweUIViewRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
