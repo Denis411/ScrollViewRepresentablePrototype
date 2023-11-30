db85b0312a083e6cdae586225627790da71c446e
Denis Ulianov <dulianov@tradingview.com>
2023-11-30 19:50:57 +0400

    Add BrokerFilterBubbleCellView

--- ScrollViweUIViewRepresentable/ContentView.swift
+++ ScrollViweUIViewRepresentable/ContentView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @Environment(\.layoutDirection) private var layoutDirection
+
     var body: some View {
         VStack {
             Text("hi")
-            ScrollViewRepresentable(bubbleNames: ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth"])
+            BrokerFilterBubbleScrollView(bubbleNames: ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth"])
                 .frame(maxHeight: 50)
         }
         .padding()
+        .environment(\.layoutDirection, .rightToLeft)
     }
 }
 
@@ -22,7 +26,7 @@ struct ContentView: View {
     ContentView()
 }
 
-struct ScrollViewRepresentable: UIViewRepresentable {
+struct BrokerFilterBubbleScrollView: UIViewRepresentable {
 
     private let bubbleNames: [String]
 
@@ -45,7 +49,7 @@ struct ScrollViewRepresentable: UIViewRepresentable {
 
 }
 
-extension ScrollViewRepresentable {
+extension BrokerFilterBubbleScrollView {
 
     private func setStackView() {
         horizontalStackView.axis = .horizontal
@@ -66,7 +70,7 @@ extension ScrollViewRepresentable {
     private func createBubbleViews() {
 
         for name in bubbleNames {
-            let bubbleView = UILabel()
+            let bubbleView = BrokerFilterBubbleCellView()
             bubbleView.text = name
             horizontalStackView.addArrangedSubview(bubbleView)
         }
@@ -77,6 +81,75 @@ extension ScrollViewRepresentable {
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.bounces = true
         scrollView.backgroundColor = .green
+    }
+
+}
+
+
+final class BrokerFilterBubbleCellView: UILabel {
+
+    private var onTapAction: (() -> Void)?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let extraWidth = Constants.insets.left + Constants.insets.right
+        let extraHeight = Constants.insets.bottom + Constants.insets.top
+
+        return CGSize(
+            width: super.intrinsicContentSize.width + extraWidth,
+            height: super.intrinsicContentSize.height + extraHeight
+        )
+    }
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: Constants.insets))
+        setUpSelf()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setUpSelf() {
+        setGestureRecognizer()
+        self.numberOfLines = 1
+        self.layer.cornerRadius = 12
+        self.textColor = .blue
+        self.layer.backgroundColor = UIColor.orange.cgColor
+    }
+
+}
+
+// MARK: - On tap actions
+extension BrokerFilterBubbleCellView {
+
+    @inline(__always)
+    private func setGestureRecognizer() {
+        isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(performOnTapAction))
+        self.addGestureRecognizer(gestureRecognizer)
+    }
+
+    public func setOnTapAction(_ onTapAction: @escaping () -> Void) {
+        self.onTapAction = onTapAction
+    }
+
+    @objc func performOnTapAction() {
+        onTapAction?()
+    }
+
+}
+
+// MARK: - Constants
+extension BrokerFilterBubbleCellView {
+
+    enum Constants {
+
+        static let insets = UIEdgeInsets(top: 7, left: 12, bottom: 7, right: 12)
+
     }
 
 }
