e6f531965edd02ce8b154cbab5a28cc433572d92
Denis Ulianov <dulianov@tradingview.com>
2023-11-30 19:18:21 +0400

    Create ScrollViewRepresentable

--- ScrollViweUIViewRepresentable/ContentView.swift
+++ ScrollViweUIViewRepresentable/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("hi")
+            ScrollViewRepresentable(bubbleNames: ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth"])
+                .frame(maxHeight: 50)
         }
         .padding()
     }
@@ -21,4 +20,63 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ScrollViewRepresentable: UIViewRepresentable {
+
+    private let bubbleNames: [String]
+
+    private let scrollView = UIScrollView()
+    private let horizontalStackView = UIStackView()
+
+    init(bubbleNames: [String]) {
+        self.bubbleNames = bubbleNames
+        setStackView()
+        setScrollView()
+    }
+
+    func makeUIView(context: Context) -> some UIView {
+        scrollView
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+
+    }
+
+}
+
+extension ScrollViewRepresentable {
+
+    private func setStackView() {
+        horizontalStackView.axis = .horizontal
+        horizontalStackView.spacing = 10
+        horizontalStackView.alignment = .leading
+        horizontalStackView.distribution = .fill
+
+        scrollView.addSubview(horizontalStackView)
+        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
+        horizontalStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        horizontalStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        horizontalStackView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        horizontalStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+
+        createBubbleViews()
+    }
+
+    private func createBubbleViews() {
+
+        for name in bubbleNames {
+            let bubbleView = UILabel()
+            bubbleView.text = name
+            horizontalStackView.addArrangedSubview(bubbleView)
+        }
+
+    }
+
+    private func setScrollView() {
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.bounces = true
+        scrollView.backgroundColor = .green
+    }
+
 }
