e5cbd075df4b46a2b1a22b04b843f1bfa8755036
Denis Ulianov <dulianov@tradingview.com>
2023-12-01 15:01:06 +0400

    Create file BrokerFilterBubbleScrollView

--- ScrollViweUIViewRepresentable/ContentView.swift
+++ ScrollViweUIViewRepresentable/ContentView.swift
@@ -37,32 +37,3 @@ struct ContentView: View {
         .padding()
     }
 }
-
-struct BrokerFilterBubbleScrollView: UIViewRepresentable {
-
-    private let chosenFilterType: BrokersFilterType
-    private let chooseFilterAction: (BrokersFilterType) -> Void
-
-    init(
-        chosenFilterType: BrokersFilterType,
-        chooseFilterAction: @escaping (BrokersFilterType) -> Void
-    ) {
-        self.chosenFilterType = chosenFilterType
-        self.chooseFilterAction = chooseFilterAction
-    }
-
-    private let scrollView = UIScrollView()
-    private let horizontalStackView = UIStackView()
-
-    func makeUIView(context: Context) -> some UIView {
-        return BrokerFilterScrollView(
-            chosenFilterType: chosenFilterType,
-            chooseFilterAction: chooseFilterAction
-        )
-    }
-
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        (uiView as? BrokerFilterScrollView)?.updateChosenFilterType(chosenFilterType)
-    }
-
-}
